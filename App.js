8cf55f2b1c7ed622142089e2c09ea8a96e7f5b17
allu <aleksijaakkojuhani@gmail.com>
2020-02-20 20:34:21 +0200

    initial

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+/* const navigator = createStackNavigator({
+
+}); */
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>juuuuukeli</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+// client id ZWU8CccgSjJcDx-4DUu92g
+// api key -VRBQGxVcT3ilzaRTUXDwrUkt39x4PsNiVVORJYjWlgzJHt5EFMEotVWZMn5nw2-NgzNNBDiK5TQf6IJNoVDegtmOVmEHrPwaobqZsyZIWouoXvPrJ9dek5xQmFNXnYx
